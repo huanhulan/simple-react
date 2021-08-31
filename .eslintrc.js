b5b217ce9fbbbc9d9e0318c8350ecdc1106afc68
huanhulan <huanhulan83@gmail.com>
2021-08-31 17:53:52 +0800

    refactor(.eslintrc.js): remove .eslintrc.js in .eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,67 +5,63 @@ module.exports = {
     node: true,
   },
   extends: [
-    "airbnb",
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
+    'airbnb',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
     // 'plugin:react/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 7,
-    ecmaFeatures: {
-      legacyDecorators: true,
-    },
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: "module",
+    sourceType: 'module',
   },
   plugins: [
-    "react",
-    "@typescript-eslint",
+    'react',
+    '@typescript-eslint',
     //  "react-hooks"
   ],
-  "settings": {
-    "import/extensions": [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"]
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
-      "node": {
-        "extensions": [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
   },
   rules: {
-    quotes: ["error", "single"],
-    "no-shadow": "off",
-    "react/jsx-filename-extension": "off",
-    "react/react-in-jsx-scope": "off",
-    "@typescript-eslint/no-shadow": "error",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "import/prefer-default-export": 0,
-    "no-param-reassign": 0,
-    "prettier/prettier": [
-      "error",
+    quotes: ['error', 'single'],
+    'no-shadow': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-namespace': 'off',
+    'import/prefer-default-export': 0,
+    'no-param-reassign': 0,
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
       },
     ],
-    "import/extensions": [
+    'import/extensions': [
       2,
-      "ignorePackages",
+      'ignorePackages',
       {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
   },
