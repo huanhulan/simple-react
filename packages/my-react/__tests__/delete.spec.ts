d967c62219ff9467bec207b5eb8523ffaccd043b
huanhulan <huanhulan83@gmail.com>
2021-10-05 20:07:32 +0800

    test(my-react): add deletion test

---
+++ packages/my-react/__tests__/delete.spec.ts
@@ -0,0 +1,55 @@
+import { fireEvent } from '@testing-library/dom';
+import { getExampleDOM } from 'test-utils';
+import { requestIdleCallback } from '@shopify/jest-dom-mocks';
+import faker from 'faker';
+import { render, createElement, useState, useCallback } from '../index';
+
+test('Check rendering of a functional component with a prop', () => {
+  const container = getExampleDOM();
+  const testData = [
+    new Array(
+      faker.random.number({
+        min: 1,
+        max: 200,
+      })
+    ),
+  ]
+    .fill(1)
+    .map(() => faker.lorem.sentence());
+  function Greeting({ contents }: { contents: string[] }) {
+    const [realContents, setRealContents] = useState(contents);
+    const removeLastLine = useCallback(() => {
+      const idx = realContents.length - 1;
+      const rest = realContents.slice(0, idx < 0 ? 0 : idx);
+      setRealContents(rest);
+    }, [setRealContents, realContents]);
+
+    return createElement('div', {}, [
+      ...realContents.map((str) => createElement('p', {}, [str])),
+      createElement(
+        'button',
+        {
+          onClick: removeLastLine,
+        },
+        ['click to delete a line']
+      ),
+    ]);
+  }
+
+  render(
+    createElement(Greeting as FunctionComponent, { contents: testData }),
+    container
+  );
+
+  expect(container.querySelectorAll('p')?.length).toBe(testData.length);
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = testData.length - 1; i > -1; i--) {
+    fireEvent(
+      container.querySelector('button') as HTMLButtonElement,
+      new MouseEvent('click')
+    );
+    requestIdleCallback.runIdleCallbacks();
+    expect(container.querySelectorAll('p')?.length).toBe(i);
+  }
+});
--- packages/my-react/jest.config.js
+++ packages/my-react/jest.config.js
@@ -150,7 +150,8 @@ export default {
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   testPathIgnorePatterns: [
     "/node_modules/",
-    "/.rollup.cache/"
+    "/.rollup.cache/",
+    '/dist/'
   ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
